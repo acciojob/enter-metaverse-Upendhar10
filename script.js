5848932772d6cfd9c22580ddb3e8655e784823fd
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-09 22:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
-let content = document.getElementById("content");
+let content = document.getElementById("status");
 const enterBtn = document.getElementById("enterBtn");
 
 enterBtn.addEventListener("click", () => {
-	content.innerText = "Entered Metaverse";
+	content.innerHTML = "<h1>Entered Metaverse</h1>";
 })
