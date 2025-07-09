dd9c32f8bad567c922d8abd15a534590229dd9eb
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-09 22:25:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let content = document.getElementById("content");
+const enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", () => {
+	content.innerText = "Entered Metaverse";
+})
